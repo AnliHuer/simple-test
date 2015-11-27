035f2a9463adc2d7604bc3893705179383dc2dcb
AnliHuer <AnliHuer@outlook.com>
2015-11-27 16:18:21 +0800

    finish the practice-2

--- src/calculate-second.js
+++ src/calculate-second.js
@@ -1,3 +1,29 @@
-function calculate_median(arr) {
+function getOddArray(arr) {
+  var oddArr = [];
 
+  for (var i = 0; i < arr.length; i++) {
+    if (i % 2 !== 0) {
+      oddArr.push(arr[i]);
+    }
+  }
+  return oddArr;
+}
+
+
+function getMeadinNum(oddArr) {
+  var medianNum;
+
+  medianNum = oddArr.length % 2 !== 0 ? oddArr[(oddArr.length - 1) / 2] : (oddArr[oddArr.length / 2] + oddArr[oddArr.length / 2 - 1]) / 2;
+  return medianNum;
+}
+
+
+function calculate_median(arr) {
+  var oddArr = getOddArray(arr);
+
+  oddArr.sort(function(a, b) {
+    return a > b;
+  });
+
+  return getMeadinNum(oddArr);
 }
